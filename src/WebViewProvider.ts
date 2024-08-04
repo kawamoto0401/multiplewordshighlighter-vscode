224d97426aa17b262d6744f0e5531fea5b122c78
Kawamoto <kawamoto0401@outlook.jp>
2024-08-04 22:08:57 +0900

    0.2.3

--- src/WebViewProvider.ts
+++ src/WebViewProvider.ts
@@ -36,16 +36,11 @@ export class WebViewProvider implements vscode.WebviewViewProvider {
     webviewView.webview.options = {
       enableScripts: true,
       // `./public` 内のファイルのみ読み込めるように制限する
-      localResourceRoots: [vscode.Uri.joinPath(this.extensionUri, 'public')]
+      localResourceRoots: [vscode.Uri.joinPath(this.extensionUri, 'media')]
     };
 
-    // WebView 内で`./public/index.css`を読み込み可能にするためのUri
-    const styleUri = webviewView.webview.asWebviewUri(
-      vscode.Uri.joinPath(this.extensionUri, "public", "index.css")
-    );
-
-    // WebView にイベントを送信します
-    webviewView.webview.postMessage({ type: "setup" });
+    // // WebView にイベントを送信します
+    // webviewView.webview.postMessage({ type: "setup" });
 
     // WebView 内からのイベントを受け取ります
     webviewView.webview.onDidReceiveMessage((data) => {
@@ -102,6 +97,10 @@ export class WebViewProvider implements vscode.WebviewViewProvider {
 		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
 		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this.extensionUri, 'media', 'main.js'));
 
+		// Do the same for the stylesheet.
+		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this.extensionUri, 'media', 'vscode.css'));
+		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this.extensionUri, 'media', 'main.css'));
+
 		// Use a nonce to only allow a specific script to be run.
 		const nonce = getNonce();
 
@@ -110,334 +109,63 @@ export class WebViewProvider implements vscode.WebviewViewProvider {
       <html lang="ja">
       <head>
         <meta charset="UTF-8">
+
+ 				<!--
+					Use a content security policy to only allow loading styles from our extension directory,
+					and only allow scripts that have a specific nonce.
+					(See the 'webview-sample' extension sample for img-src content security policy examples)
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>WebView Example</title>
+
+  			<link href="${styleVSCodeUri}" rel="stylesheet">
+				<link href="${styleMainUri}" rel="stylesheet">
+   
       </head>
       <body>
 
-      <div>
+      <div class="box">
         <input type="text" id="input1" name="input1" size="30" />
-        <br>
         <input type="checkbox" name="option" id="checkbox1_1">
         <label for="checkbox1_1">小/大文字</label>
         <input type="checkbox" name="option" id="checkbox1_2">
         <label for="checkbox1_2">単語検索</label>
       </div>
 
-      <div>
+      <div class="box">
         <input type="text" id="input2" name="input2" size="30" />
-        <br>
         <input type="checkbox" name="option" id="checkbox2_1">
         <label for="checkbox2_1">小/大文字</label>
         <input type="checkbox" name="option" id="checkbox2_2">
         <label for="checkbox2_2">単語検索</label>
       </div>
 
-      <div>
+      <div class="box">
         <input type="text" id="input3" name="input3" size="30" />
-        <br>
         <input type="checkbox" name="option" id="checkbox3_1">
         <label for="checkbox3_1">小/大文字</label>
         <input type="checkbox" name="option" id="checkbox3_2">
         <label for="checkbox3_2">単語検索</label>
       </div>
 
-      <div>
+      <div class="box">
         <input type="text" id="input4" name="input4" size="30" />
-        <br>
         <input type="checkbox" name="option" id="checkbox4_1">
         <label for="checkbox4_1">小/大文字</label>
         <input type="checkbox" name="option" id="checkbox4_2">
         <label for="checkbox4_2">単語検索</label>
       </div>
 
-      <div>
+      <div class="box">
         <input type="text" id="input5" name="input5" size="30" />
-        <br>
         <input type="checkbox" name="option" id="checkbox5_1">
         <label for="checkbox5_1">小/大文字</label>
         <input type="checkbox" name="option" id="checkbox5_2">
         <label for="checkbox5_2">単語検索</label>
       </div>
 
-      <script>
-          function butotnClick(){
-            console.log("Debug:butotnClick");
-
-            let check_1_1 = false;
-            if (document.getElementById('checkbox1_1').checked){
-              // チェックあり の場合
-              check_1_1 = true;
-            }
-
-            let check_1_2 = false;
-            if (document.getElementById('checkbox1_2').checked){
-              // チェックあり の場合
-              check_1_2 = true;
-            }
-
-            let check_2_1 = false;
-            if (document.getElementById('checkbox2_1').checked){
-              // チェックあり の場合
-              check_2_1 = true;
-            }
-
-            let check_2_2 = false;
-            if (document.getElementById('checkbox2_2').checked){
-              // チェックあり の場合
-              check_2_2 = true;
-            }
-
-            let check_3_1 = false;
-            if (document.getElementById('checkbox3_1').checked){
-              // チェックあり の場合
-              check_3_1 = true;
-            }
-
-            let check_3_2 = false;
-            if (document.getElementById('checkbox3_2').checked){
-              // チェックあり の場合
-              check_3_2 = true;
-            }
-            
-            let check_4_1 = false;
-            if (document.getElementById('checkbox4_1').checked){
-              // チェックあり の場合
-              check_4_1 = true;
-            }
-
-            let check_4_2 = false;
-            if (document.getElementById('checkbox4_2').checked){
-              // チェックあり の場合
-              check_4_2 = true;
-            }
-
-            let check_5_1 = false;
-            if (document.getElementById('checkbox5_1').checked){
-              // チェックあり の場合
-              check_5_1 = true;
-            }
-
-            let check_5_2 = false;
-            if (document.getElementById('checkbox5_2').checked){
-              // チェックあり の場合
-              check_5_2 = true;
-            }              
-            
-            const obj = {
-              "searchData": [
-                {
-                  "name": reviewTextarea_1.value,
-                  "uppercaseLetter": check_1_1,
-                  "wordSearch": check_1_1
-                },
-                {
-                  "name": reviewTextarea_2.value,
-                  "uppercaseLetter": check_2_1,
-                  "wordSearch": check_2_2
-                },
-                {
-                  "name": reviewTextarea_3.value,
-                  "uppercaseLetter": check_3_1,
-                  "wordSearch": check_3_2
-                },
-                {
-                  "name": reviewTextarea_4.value,
-                  "uppercaseLetter": check_4_1,
-                  "wordSearch": check_4_2
-                },
-                {
-                  "name": reviewTextarea_5.value,
-                  "uppercaseLetter": check_5_1,
-                  "wordSearch": check_5_2
-                },
-                
-                ]
-            };
-
-            let json = JSON.stringify(obj);
-
-            // 拡張側にイベントを送信します
-            vscode.postMessage({ type: "change-event", text: json})
-
-            updateColorList(obj);
-
-            // Update the saved state
-            vscode.setState(json);
-
-            console.log("Debug:butotnClick End");
-          }
-
-          function updateColorList(obj) {
-            reviewTextarea_1.value = obj.searchData[0].name;
-            reviewTextarea_2.value = obj.searchData[1].name;
-            reviewTextarea_3.value = obj.searchData[2].name;
-            reviewTextarea_4.value = obj.searchData[3].name;
-            reviewTextarea_5.value = obj.searchData[4].name;
-
-            if( obj.searchData[0].uppercaseLetter ) {
-              reviewcheckbox1_1.checked = true;
-            }
-            if( obj.searchData[0].wordSearch ) {
-              reviewcheckbox1_2.checked = true;
-            }
-
-            if( obj.searchData[1].uppercaseLetter ) {
-              reviewcheckbox2_1.checked = true;
-            }
-            if( obj.searchData[1].wordSearch ) {
-              reviewcheckbox2_2.checked = true;
-            }
- 
-            if( obj.searchData[2].uppercaseLetter ) {
-              reviewcheckbox3_1.checked = true;
-            }
-            if( obj.searchData[2].wordSearch ) {
-              reviewcheckbox3_2.checked = true;
-            }
-
-            if( obj.searchData[3].uppercaseLetter ) {
-              reviewcheckbox4_1.checked = true;
-            }
-            if( obj.searchData[3].wordSearch ) {
-              reviewcheckbox4_2.checked = true;
-            }
-              
-            if( obj.searchData[4].uppercaseLetter ) {
-              reviewcheckbox5_1.checked = true;
-            }
-            if( obj.searchData[4].wordSearch ) {
-              reviewcheckbox5_2.checked = true;
-            }              
-          }
-
-          const vscode = acquireVsCodeApi();
-          const oldState = vscode.getState() || {
-                                                  "searchData": [
-                                                    {
-                                                      "name": "",
-                                                      "uppercaseLetter": false,
-                                                      "wordSearch": false
-                                                    },
-                                                    {
-                                                      "name": "",
-                                                      "uppercaseLetter": false,
-                                                      "wordSearch": false
-                                                    },
-                                                    {
-                                                      "name": "",
-                                                      "uppercaseLetter": false,
-                                                      "wordSearch": false
-                                                    },
-                                                    {
-                                                      "name": "",
-                                                      "uppercaseLetter": false,
-                                                      "wordSearch": false
-                                                    },
-                                                    {
-                                                      "name": "",
-                                                      "uppercaseLetter": false,
-                                                      "wordSearch": false
-                                                    },
-                                                    ]
-                                                };
-          let json;
-          if( typeof oldState == 'string') {
-            try {
-              json = JSON.parse(oldState);
-              
-            } catch (e) {
-            }          
-          } 
-
-          const HTMLElements = [8];
- 
-          let reviewTextarea_1 = document.getElementById('input1');
-          reviewTextarea_1.addEventListener('change', butotnClick);
-
-          let reviewcheckbox1_1 = document.getElementById('checkbox1_1');
-          reviewcheckbox1_1.addEventListener('click', butotnClick);
-
-          let reviewcheckbox1_2 = document.getElementById('checkbox1_2');
-          reviewcheckbox1_2.addEventListener('click', butotnClick);
-          
-          
-          let reviewTextarea_2 = document.getElementById('input2');
-          reviewTextarea_2.addEventListener('change', butotnClick);
-
-          let reviewcheckbox2_1 = document.getElementById('checkbox2_1');
-          reviewcheckbox2_1.addEventListener('click', butotnClick);
-
-          let reviewcheckbox2_2 = document.getElementById('checkbox2_2');
-          reviewcheckbox2_2.addEventListener('click', butotnClick);
-
-          
-          let reviewTextarea_3 = document.getElementById('input3');
-          reviewTextarea_3.addEventListener('change', butotnClick);
-
-          let reviewcheckbox3_1 = document.getElementById('checkbox3_1');
-          reviewcheckbox3_1.addEventListener('click', butotnClick);
-
-          let reviewcheckbox3_2 = document.getElementById('checkbox3_2');
-          reviewcheckbox3_2.addEventListener('click', butotnClick);
-
-          
-          let reviewTextarea_4 = document.getElementById('input4');
-          reviewTextarea_4.addEventListener('change', butotnClick);
-
-          let reviewcheckbox4_1 = document.getElementById('checkbox4_1');
-          reviewcheckbox4_1.addEventListener('click', butotnClick);
-
-          let reviewcheckbox4_2 = document.getElementById('checkbox4_2');
-          reviewcheckbox4_2.addEventListener('click', butotnClick);
-
-
-          let reviewTextarea_5 = document.getElementById('input5');
-          reviewTextarea_5.addEventListener('change', butotnClick);     
-
-          let reviewcheckbox5_1 = document.getElementById('checkbox5_1');
-          reviewcheckbox5_1.addEventListener('click', butotnClick);
-
-          let reviewcheckbox5_2 = document.getElementById('checkbox5_2');
-          reviewcheckbox5_2.addEventListener('click', butotnClick);
-
-
-          updateColorList(json);
-
-          try {
-            // 拡張側からのイベントを受け取ります
-            window.addEventListener("message", (event) => {
-
-              if(event.data.type === "setup") {
-                const data = event.data.data;
-                switch (data.index){
-                  case 0:
-                    reviewTextarea_1.value = data.message;
-                    break;
-                  case 1:
-                    reviewTextarea_2.value = data.message;
-                    break;
-                  case 2:
-                    reviewTextarea_3.value = data.message;
-                    break;
-                  case 3:
-                    reviewTextarea_4.value = data.message;
-                    break;
-                  case 4:
-                    reviewTextarea_5.value = data.message;
-                    break;
-                  default:
-                    break;
-                }
-
-                butotnClick();
-              }
-            });
-            } catch (e) {
-          }
-
-
-      </script>
+      <script nonce="${nonce}" src="${scriptUri}"></script>
 
       </body>
 
@@ -466,3 +194,15 @@ function getNonce() {
 }
 
 
+// <!--
+// コンテンツセキュリティポリシーを使用して、拡張機能ディレクトリからのスタイルの読み込みのみを許可します。
+// また、特定の nonce を持つスクリプトのみを許可します。
+// -->
+// <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+
+// <link href="${styleVSCodeUri}" rel="stylesheet">
+// <link href="${styleMainUri}" rel="stylesheet">
+
+// <title>WebView Example</title>
+
+// <script nonce="${nonce}" src="${scriptUri}"></script>
